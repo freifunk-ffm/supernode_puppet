54288c5c7b4ccf415d7dcd1b613586661845e513
kaleng <kaleng@gokk.de>
2014-06-20 19:00:39 +0200

    split augeas definitions for unbound

--- modules/unbound/manifests/init.pp
+++ modules/unbound/manifests/init.pp
@@ -22,19 +22,30 @@ class unbound {
     ensure    => running,
     enable    => true,
     hasstatus => true,
-    require   => [Package['unbound'], File['unbound.conf'], Augeas['unbound']],
+    require   => [
+      Package['unbound'], 
+      File['unbound.conf'], 
+      Augeas['iface lo post-up'],
+      Augeas['iface bat0:0 inet'],
+    ],
   }
 
-  augeas { 'unbound':
+  augeas { 'iface lo post-up':
     context => '/files/etc/network/interfaces',
     changes => [
       'set iface[. = "lo"]/post-up "ip -6 addr add fdd3:5d16:b5dd::2/128 dev lo"',
+    ]
+  }
+
+  augeas { 'iface bat0:0 inet':
+    context => '/files/etc/network/interfaces',
+    changes => [
       'set auto[child::1 = "bat0:0"]/1 bat0:0',
       'set iface[. = "bat0:0"] bat0:0',
       'set iface[. = "bat0:0"]/family inet',
       'set iface[. = "bat0:0"]/method static',
       'set iface[. = "bat0:0"]/address 172.27.0.2',
       'set iface[. = "bat0:0"]/netmask 255.255.192.0',
-    ]
-  }
+    ],
+  } 
 }
