9fb9cb61d9d37faeb3e1d3d4899019592bd4cb21
kaleng <kaleng@gokk.de>
2014-06-21 15:19:01 +0200

    move bat0 definitions to /etc/network/interfaces

--- modules/batman/manifests/init.pp
+++ modules/batman/manifests/init.pp
@@ -14,35 +14,41 @@ class batman {
     require => Package['batman-adv-dkms'],
   }
 
+  package { 'uml-utilities':
+    ensure  => installed,
+  }
+
   exec { 'modprobe batman':
     command => '/sbin/modprobe batman-adv',
     unless  => '/bin/grep -q batman_adv /proc/modules',
     require => Package['batman-adv-dkms'],
   }
 
-#  augeas { 'iface bat0 inet':
-#    context => '/files/etc/network/interfaces',
-#    changes => [
-#      'set auto[child::1 = "bat0"]/1 bat0',
-#      'set iface[. = "bat0"] bat0',
-#      'set iface[. = "bat0"]/family inet',
-#      'set iface[. = "bat0"]/method static',
-#      "set iface[. = \"bat0\"]/address 172.27.${ipv4_subnet_start}.${ipv4_suffix}",
-#      'set iface[. = "bat0"]/netmask 255.255.192.0',
-#    ],
-#    require => Exec['modprobe batman'],
-#    notify  => Augeas['iface bat0 inet6']
-#  }
-#
-#  augeas { 'iface bat0 inet6':
-#    context => '/files/etc/network/interfaces',
-#    changes => [
-#      'set iface[. = "bat0.6"] bat0.6',
-#      'set iface[. = "bat0.6"]/family inet6',
-#      'set iface[. = "bat0.6"]/method static',
-#      "set iface[. = \"bat0.6\"]/address 2001:67c:20a0:${ipv6_subnet}::1",
-#      'set iface[. = "bat0.6"]/netmask 64',
-#      'set iface[. = "bat0.6"] bat0',
-#    ],
-#  } 
+  augeas { 'iface bat0 inet':
+    context => '/files/etc/network/interfaces',
+    changes => [
+      'set auto[child::1 = "bat0"]/1 bat0',
+      'set iface[. = "bat0"] bat0',
+      'set iface[. = "bat0"]/family inet',
+      'set iface[. = "bat0"]/method static',
+      "set iface[. = \"bat0\"]/address 172.27.${ipv4_subnet_start}.${ipv4_suffix}",
+      'set iface[. = "bat0"]/netmask 255.255.192.0',
+      'set iface[. = "bat0"]/pre-up "modprobe batman-adv && tunctl -t mesh-vpn && batctl if add mesh-vpn"',
+    ],
+    require => Package['uml-utilities'],
+    notify  => Augeas['iface bat0 inet6']
+  }
+
+  augeas { 'iface bat0 inet6':
+    context => '/files/etc/network/interfaces',
+    changes => [
+      'set iface[. = "bat0.6"] bat0.6',
+      'set iface[. = "bat0.6"]/family inet6',
+      'set iface[. = "bat0.6"]/method static',
+      "set iface[. = \"bat0.6\"]/address 2001:67c:20a0:${ipv6_subnet}::1",
+      'set iface[. = "bat0.6"]/netmask 64',
+      'set iface[. = "bat0.6"] bat0',
+    ],
+    onlyif  => 'match iface[. = "bat0"][family = "inet6"] size == 0',
+  } 
 }
