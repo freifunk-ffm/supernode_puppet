53c515a402685c3ad90f76338ab367ed0b76664c
root <root@fastd2>
2014-11-02 00:24:32 +0100

    fix added parameters openvpn

--- openvpn/manifests/init.pp
+++ openvpn/manifests/init.pp
@@ -12,7 +12,7 @@ package { 'openvpn': ensure => installed; }
 
   file { ['/etc/openvpn/']:
     ensure => directory,
-    notify => [File['openvpn-default'], File['openvpn_vpn-up'], File['openvpn_vpn-route'],Exec['openvpn_config_1'], Exec['openvpn_config_2'], Exec['openvpn_config_3'], Exec['openvpn_config_4'],  Exec['openvpn_config_5'] ],
+    notify => [File['openvpn-default'], File['openvpn_vpn-up'], File['openvpn_vpn-route'],Exec['openvpn_config_1'], Exec['openvpn_config_2'], Exec['openvpn_config_3'], Exec['openvpn_config_4'],  Exec['openvpn_config_5'], Exec['openvpn_config_6'] ],
     require => Package['openvpn'],
   }
 
@@ -32,25 +32,29 @@ package { 'openvpn': ensure => installed; }
     notify  => Service['openvpn'],
   }
 
+  exec { 'openvpn_config_6':
+    command => '/bin/echo "dev-type tun" >> /etc/openvpn/ovpn-inet.conf',
+    unless  => '/bin/grep "dev-type tun" /etc/openvpn/ovpn-inet.conf',
+  }
   exec { 'openvpn_config_5':
-    command => '/bin/echo "dev ovpn-inet;dev-type tun" > /etc/openvpn/ovpn-inet.conf',
+    command => '/bin/echo "dev ovpn-inet" >> /etc/openvpn/ovpn-inet.conf',
     unless  => '/bin/grep "dev ovpn-inet" /etc/openvpn/ovpn-inet.conf',
   }
   exec { 'openvpn_config_1':
-    command => '/bin/echo "route-up vpn-route-up.sh" > /etc/openvpn/ovpn-inet.conf',
+    command => '/bin/echo "route-up vpn-route-up.sh" >> /etc/openvpn/ovpn-inet.conf',
     unless  => '/bin/grep "route-up vpn-route-up.sh" /etc/openvpn/ovpn-inet.conf',
   }
   exec { 'openvpn_config_2':
-    command => '/bin/echo "route-noexec" > /etc/openvpn/ovpn-inet.conf',
+    command => '/bin/echo "route-noexec" >> /etc/openvpn/ovpn-inet.conf',
     unless  => '/bin/grep "route-noexec" /etc/openvpn/ovpn-inet.conf',
   }
 
   exec { 'openvpn_config_3':
-    command => '/bin/echo "up vpn-up.sh" > /etc/openvpn/ovpn-inet.conf',
+    command => '/bin/echo "up vpn-up.sh" >> /etc/openvpn/ovpn-inet.conf',
     unless  => '/bin/grep "up vpn-up.sh" /etc/openvpn/ovpn-inet.conf',
   }
   exec { 'openvpn_config_4':
-    command => '/bin/echo "script-security 2" > /etc/openvpn/ovpn-inet.conf',
+    command => '/bin/echo "script-security 2" >> /etc/openvpn/ovpn-inet.conf',
     unless  => '/bin/grep "script-security 2" /etc/openvpn/ovpn-inet.conf',
   }
 
