6d85b6bf801003aef93afa2b8727cb90019c27f4
Christof Schulze <christof.schulze@gmx.net>
2014-11-16 21:56:37 +0100

    first try for postfix module

--- postfix/manifests/init.pp
+++ postfix/manifests/init.pp
@@ -1,5 +1,7 @@
-class postfix ($fastd_key) {
+class postfix ($fastd_key, $fastd_num) {
   package { 'postfix': ensure => installed, }
+  package { 'pwgen': ensure => installed, }
+
   service { 'postfix':
     ensure => running,
     enable => true,
@@ -13,4 +15,10 @@ class postfix ($fastd_key) {
     content => template('postfix/main.cf.erb'),
     notify => Service['tinc'],
   }
+  #"[mail.bb.ffm.freifunk.net] user:pass; postmap file
+  exec { 'openvpn_config_6':
+  command => 'pw=$(pwgen 10); /bin/echo "[mail.bb.ffm.freifunk.net]  fastd$fastd_num:$pw" >> /etc/postfix/sasl_passwd; postmap /etc/postfix/sasl_passwd; /bin/echo "MAKE SURE TO ADD fastd$fastd_num:$pw on mail.bb.ffm.freifunk.net TO /etc/postfix/sasld_passwd',
+      unless  => '/bin/grep "mail.bb.ffm.freifunk.net" /etc/postfix/sasl_passwd',
+  }
+
 }
