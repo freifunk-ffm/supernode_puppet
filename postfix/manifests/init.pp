c6985789206e9c979bfdbb3b5160e8178dd1885f
root <root@fastd1.hdj.ffm.freifunk.net>
2014-11-26 02:01:07 +0100

    postfix module fixes - however still manual passwd

--- postfix/manifests/init.pp
+++ postfix/manifests/init.pp
@@ -7,7 +7,7 @@ class postfix ($fastd_key, $fastd_num) {
     enable => true,
     hasrestart => true,
     hasstatus => false,
-    require => Package['postfix', 'pwgen'],
+    require => [ Package['postfix'], Package['pwgen'] ],
   }
   file { 'main-cf':
     ensure => file,
@@ -17,8 +17,10 @@ class postfix ($fastd_key, $fastd_num) {
   }
   #"[mail.bb.ffm.freifunk.net] user:pass; postmap file
   exec { 'postfix_config_6':
-  command => 'echo pw=$(pwgen 10); /bin/echo "[mail.bb.ffm.freifunk.net]  fastd$fastd_num:$pw" > /etc/postfix/sasl_passwd; postmap /etc/postfix/sasl_passwd; /bin/echo "MAKE SURE TO ADD fastd$fastd_num:$pw on mail.bb.ffm.freifunk.net TO /etc/postfix/sasld_passwd"',
-      unless  => '/bin/grep "mail.bb.ffm.freifunk.net" /etc/postfix/sasl_passwd'
+  command => '/bin/bash -lc "echo \"[mail.bb.ffm.freifunk.net]  fastd$fastd_num:$(/usr/bin/pwgen 10 -1)\" > /etc/postfix/sasl_passwd; /usr/sbin/postmap /etc/postfix/sasl_passwd;"',
+  path => "['/usr/bin','/bin', '/usr/sbin']",
+      unless  => '/bin/grep mail.bb.ffm.freifunk.net /etc/postfix/sasl_passwd'
   }
+notify {"MAKE SURE TO ADD fastd$fastd_num:PASWORD (see /etc/postfix/sasl_passwd) to /etc/postfix/sasld_passwd on mail.bb.ffm.freifunk.net and run postmap /etc/postfix/sasld_passwd":}
 
 }
