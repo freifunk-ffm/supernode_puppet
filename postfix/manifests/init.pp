f38a50fa40b9a3a02bc20b1c4a3987c20b4f2627
Christof Schulze <christof.schulze@gmx.net>
2015-03-05 20:16:32 +0100

    change order of postfix config changes

--- postfix/manifests/init.pp
+++ postfix/manifests/init.pp
@@ -13,18 +13,22 @@ class postfix () {
     ensure => file,
     path => '/etc/postfix/main.cf',
     content => template('postfix/main.cf.erb'),
-#    notify => Service['tinc'],
+    notify => Service['postfix'],
   }
   #"[mail.bb.ffm.freifunk.net] user:pass; postmap file
   exec { 'postfix_config_6':
-  command => '/bin/bash -c "echo \"[mail.bb.ffm.freifunk.net]  $(/bin/hostname -s):$(/usr/bin/pwgen 10 -1)\" > /etc/postfix/sasl_passwd; /usr/sbin/postmap /etc/postfix/sasl_passwd;"',
-  path => "['/usr/bin','/bin', '/usr/sbin']",
+    command => '/bin/bash -c "echo \"[mail.bb.ffm.freifunk.net]  $(/bin/hostname -s):$(/usr/bin/pwgen 10 -1)\" > /etc/postfix/sasl_passwd; /usr/sbin/postmap /etc/postfix/sasl_passwd;"',
+    path => "['/usr/bin','/bin', '/usr/sbin']",
       unless  => '/bin/grep mail.bb.ffm.freifunk.net /etc/postfix/sasl_passwd'
+    require => [ Package['postfix'], Package['pwgen'] ],
+    notify => Service['postfix'],
   }
   
   exec { 'postfix_config_7':
-  command => '/bin/sed -i "/root:/d" /etc/aliases; /bin/bash -lc "echo \"root: admin@ffm.freifunk.net\"  >> /etc/aliases; newaliases"',
-  path => "['/usr/bin','/bin', '/usr/sbin']",
+    command => '/bin/sed -i "/root:/d" /etc/aliases; /bin/bash -lc "echo \"root: admin@ffm.freifunk.net\"  >> /etc/aliases; newaliases"',
+    path => "['/usr/bin','/bin', '/usr/sbin']",
+    require => [ Package['postfix'], Package['pwgen'] ],
+    notify => Service['postfix'],
   }
 notify {"MAKE SURE TO run doveadm pw -ssha enter the PASSWORD and put $(/bin/hostname -s) into /etc/dovecot/passwd on mail.bb.ffm.freifunk.net":}
 
