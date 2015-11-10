1a5ba0cfb24069ddce9577ac487cd907a0d3edbd
Thomas Weißschuh <thomas@t-8ch.de>
2015-11-10 21:31:46 +0000

    some stuff

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,11 @@ node 'puppet.ffm.freifunk.net' {
     master => true,
   }
 }
+
+node /fastd\d+\.ffm\.freifunk\.net/ {
+  class { 'ffmff::supernode':
+    supernodenum => 3,
+    fastd_key    => '0000000000000000000000000000000000000000000000000000000000000000000',
+    rndmac       => fqdn_rand(99)
+  }
+}
