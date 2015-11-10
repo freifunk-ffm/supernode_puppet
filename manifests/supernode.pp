1677b81da06485493fc299ff64dbe5b523799868
Thomas Weißschuh <thomas@t-8ch.de>
2015-11-10 21:33:55 +0000

    ...

--- manifests/supernode.pp
+++
@@ -1,5 +0,0 @@
-class { 'ffmff::supernode':
-  supernodenum           => 3,
-  fastd_key              => '0000000000000000000000000000000000000000000000000000000000000000000',
-  rndmac                 => fqdn_rand(99)
-}
