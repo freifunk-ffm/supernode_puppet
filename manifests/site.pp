65a9b4a0f4f92d96781d9c8fa2be587785a2b496
Thomas Weißschuh <thomas@t-8ch.de>
2015-11-23 21:58:06 +0000

    add manifest

---
+++ manifests/site.pp
@@ -0,0 +1,12 @@
+node /fastd\d+\.ffm\.freifunk\.net/ {
+  $certname = $::certname
+
+  $supernodenum = regsubst($certname,'^fastd(\d+).*','\1')
+  $fastd_key = trocla_get("fastd/${certname}/key")
+
+  class { 'ffmff::supernode':
+    supernodenum => $supernodenum,
+    fastd_key    => $fastd_key,
+    rndmac       => fqdn_rand(99),
+  }
+}
