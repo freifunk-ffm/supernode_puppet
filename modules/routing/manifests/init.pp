52b16c7fafa4f4904d8063e8cbc6048eb303f1c1
Christof Schulze <christof.schulze@gmx.net>
2016-02-10 22:19:33 +0100

    install ipset

--- modules/routing/manifests/init.pp
+++ modules/routing/manifests/init.pp
@@ -1,5 +1,10 @@
 class routing {
   # FIXME really replace the whole file?
+  package { 'ipset':
+    ensure => installed,
+  }
+
+
   file { '/etc/iproute2/rt_tables':
     content => template('routing/rt_tables'),
   }
