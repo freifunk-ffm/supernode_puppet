499e851c2b6dca04f01aa6dfc2926d244c7a8d06
Christof Schulze <christof.schulze@gmx.net>
2016-05-13 19:04:03 +0200

    syntax.

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,10 +14,12 @@ node /fastd\d+\.ffm\.freifunk\.net/ {
 }
 
 node /lxc\d+\.ffm.freifunk.net/ {
+  $certname = $::trusted['certname']
   $lxcnum = regsubst($certname,'^lxc(\d+).*','\1')
   class { 'ffmff::lxc':
 	lxcnum => $lxcnum,
 	rndmac => fqdn_rand(99),
+  }
 }
 
 node 'mail.ffm.freifunk.net' {
