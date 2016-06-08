82b56a2d4afe8e4a4038bb0b0546419682721930
Thomas Weißschuh <thomas@t-8ch.de>
2016-06-08 21:28:32 +0000

    ../..

--- modules/systemd/manifests/networkd.pp
+++ modules/systemd/manifests/networkd.pp
@@ -1,6 +1,13 @@
 class systemd::networkd {
   $service = 'systemd-networkd'
 
+  file { '/etc/systemd/network':
+    ensure => directory,
+    purge => true,
+    recurse => true,
+    force => true,
+  }
+
   service { $service:
     ensure   => running,
     enable   => true,
