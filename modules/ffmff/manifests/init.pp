05d720391b6667c38f0d52564f3fb2086fdd436f
Thomas Weißschuh <thomas@t-8ch.de>
2016-06-09 20:14:29 +0000

    Revert "key..."
    This reverts commit 194f032f4f92000d25f2fda484cf38f5e6244e81.

--- modules/ffmff/manifests/init.pp
+++ modules/ffmff/manifests/init.pp
@@ -16,10 +16,4 @@ class ffmff (
   include ::ffmff::admintools
   include ::ffmff::locales
   include ::ffmff::timezone
-
-  ssh_authorized_key { 'freifunk@t-8ch.de':
-    user => 'root',
-    type => 'ssh-rsa',
-    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCuGRPkEjqUhjvnpif8bjOFXujxgM7lbwL3ojisDAF/5WQ8SbAByIZaiOcGgpbYG/kF+fNXpOgKqxR3mf9dqggQDv0/jBAbUTK2027TqTFwCiksivKYX3Ciu9WM6crBfO09jLuP8VMyUD8vGjG/ewiXr1EQ508N43mwJLiFzeZvTW0it1MKuWu66BYRNz0V/Vm/FnO4ptJ3g69cVRuuL2Zbp5Vz5BSm9MNTbq3Og/gjI7lfuvjFh57GlXmiEvKfzHrrrr9OCnPP8ROfMmPpiCcmVDKIRDUfq5rVra4QLitEtkiIZ6VHSVMvmsIIP65Yj3eNkCpRXEwXsALp7D02/duB',
-  }
 }
