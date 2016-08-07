18fe5596a08fff1b2ab99b1d11ba04d4fd7497b1
skorpy2009 <magnus@sofort-surf.de>
2016-08-07 21:46:35 +0200

    tabs...

--- modules/ffmff/manifests/locales.pp
+++ modules/ffmff/manifests/locales.pp
@@ -1,9 +1,9 @@
 class ffmff::locales {
-  class { '::locales':
-    default_locale => 'en_US.UTF-8',
-    locales        => [
-      'en_US.UTF-8 UTF-8',
-      'de_DE.UTF-8 UTF-8'
-    ],
-  }
+	class { '::locales':
+		default_locale => 'en_US.UTF-8',
+		locales        => [
+			'en_US.UTF-8 UTF-8',
+			'de_DE.UTF-8 UTF-8'
+		],
+	}
 }
