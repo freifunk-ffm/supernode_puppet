feb1264e95c3456e59f5d3d5e0d1fb84271e0535
Christof Schulze <christof.schulze@gmx.net>
2016-11-22 22:09:01 +0100

    secure ntp against monlist attack

--- modules/ffmff/manifests/ntp.pp
+++ modules/ffmff/manifests/ntp.pp
@@ -1,6 +1,8 @@
 class ffmff::ntp {
    class { '::ntp':
      servers  => [ '1.ntp.services.ffffm', 'ntp.services.ffffm' ],
-     restrict => ['10.126.0.0/24'],
+     restrict => ['10.126.0.0/24', 'default limited kod nomodify notrap nopeer noquery', '-6 default limited kod nomodify notrap nopeer noquery' ],
+     disable_monitor => true,
    }
 }
+
