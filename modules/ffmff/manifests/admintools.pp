a1c461d207c94bf823428c56b35f87002600b52c
skorpy2009 <magnus@sofort-surf.de>
2017-01-08 16:08:28 +0100

    add dnstop

--- modules/ffmff/manifests/admintools.pp
+++ modules/ffmff/manifests/admintools.pp
@@ -1,6 +1,12 @@
 class ffmff::admintools {
   package { [
-    'vim', 'iftop', 'htop', 'tmux', 'tcpdump', 'iputils-tracepath',
+    'vim', 
+    'iftop', 
+    'htop', 
+    'tmux', 
+    'tcpdump', 
+    'iputils-tracepath',
+    'dnstop',
   ]:
     ensure => installed,
   }
